bfe99f409f62947deb79ea7c23fbc6c69a8bcce6
Manoj Sharma <manoj.sharma@mercedes-benz.io>
2020-08-02 00:32:33 +0200

    Updates

---
+++ precache-manifest.3c3954663cbda8a6c6ac32dd77e77878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22efb850ad24676f462fdaf693fdd4a9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d8b4bca928d8bb142b94",
+    "url": "/my-app/static/css/2.b756e824.chunk.css"
+  },
+  {
+    "revision": "b93d7201be4971fe5daa",
+    "url": "/my-app/static/css/main.12a749fe.chunk.css"
+  },
+  {
+    "revision": "d8b4bca928d8bb142b94",
+    "url": "/my-app/static/js/2.3b141762.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/my-app/static/js/2.3b141762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93d7201be4971fe5daa",
+    "url": "/my-app/static/js/main.433e7a78.chunk.js"
+  },
+  {
+    "revision": "b7b54dfbdd0fa6ac4f04",
+    "url": "/my-app/static/js/runtime-main.b9efdca9.js"
+  }
+]);
